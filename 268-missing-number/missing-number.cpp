67b2a8a471b94337bbe5c57a621065e6d5c8482e
vd-93 <vedantvd07@gmail.com>
2025-07-09 14:18:13 +0530

    Time: 16 ms (9.71%) | Memory: 21.8 MB (70.54%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -14,14 +14,35 @@ public:
     // }
 
     // M-2 : XOR
-    int missingNumber(vector<int>& nums) {
-        int ans = 0;
+    // int missingNumber(vector<int>& nums) {
+    //     int ans = 0;
 
-        for(int i = 0; i < nums.size(); i++) {
-            ans ^= i+1;
-            ans ^= nums[i];
+    //     for(int i = 0; i < nums.size(); i++) {
+    //         ans ^= i+1;
+    //         ans ^= nums[i];
+    //     }
+
+    //     return ans;
+    // }
+
+    // M-3 : Binary search
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());  // Required for binary search
+        int left = 0, right = nums.size();
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > mid) {
+                // missing number is on the left
+                right = mid;
+            } else {
+                // nums[mid] == mid, search in right half
+                left = mid + 1;
+            }
         }
 
-        return ans;
+        // left is the first index where nums[i] != i
+        return left;
     }
 };
