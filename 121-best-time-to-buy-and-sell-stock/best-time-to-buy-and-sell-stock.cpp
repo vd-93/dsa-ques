6583ae0257e4b3a936963e9be0b828bf1cc84f60
vd-93 <vedantvd07@gmail.com>
2025-07-19 00:38:28 +0530

    Time: 0 ms (100.00%) | Memory: 97.2 MB (99.01%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -12,41 +12,43 @@ public:
     //     return profit;
     // }
 
-    // M-2
-    // int maxProfit(vector<int>& prices) {
-        // int min = INT_MAX;
-        // int maxProfit = 0;
-
-        // for(int i = 0; i < prices.size(); i++) {
-        //     if(prices[i] < min) {
-        //         min = prices[i];
-        //     }
-        //     maxProfit = max(maxProfit, prices[i] - min);
-        // }
-        // return maxProfit;
-    // }
-
-    // M-3 : buy index, sell index
+    // M-2 (Optimal)
     int maxProfit(vector<int>& prices) {
+        int min = INT_MAX;
         int maxProfit = 0;
-        // Initialize buy index to 0
-        int buyI = 0;
-        int sellI = 0;
 
-        for (int i = 0; i < prices.size(); i++) {
-            if (prices[i] - prices[buyI] > maxProfit) {
-                maxProfit = prices[i] - prices[buyI];
-                // assigning the selling day's index
-                sellI = i;
+        for(int i = 0; i < prices.size(); i++) {
+            if(prices[i] < min) {
+                min = prices[i];
+                // as the buy and sell day would be the same here, as we have set min to prices[i] here, so we are not considering this day for sell
+                continue;
             }
-
-            // If ith day's price is lower than the buyI th day -> assign buyI =
-            // i
-            if (prices[i] < prices[buyI]) {
-                buyI = i;
-            }
+            maxProfit = max(maxProfit, prices[i] - min);
         }
-
         return maxProfit;
     }
+
+    // M-3 : buy index, sell index (Not for interview, just the approach that I thought of while solving the question, the approach is similar to M-2)
+    // int maxProfit(vector<int>& prices) {
+    //     int maxProfit = 0;
+    //     // Initialize buy index to 0
+    //     int buyI = 0;
+    //     int sellI = 0;
+
+    //     for (int i = 0; i < prices.size(); i++) {
+    //         if (prices[i] - prices[buyI] > maxProfit) {
+    //             maxProfit = prices[i] - prices[buyI];
+    //             // assigning the selling day's index
+    //             sellI = i;
+    //         }
+
+    //         // If ith day's price is lower than the buyI th day -> assign buyI =
+    //         // i
+    //         if (prices[i] < prices[buyI]) {
+    //             buyI = i;
+    //         }
+    //     }
+
+    //     return maxProfit;
+    // }
 };
