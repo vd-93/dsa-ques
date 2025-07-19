83ca78758111ae571984584f3620fd0d8560cfdd
vd-93 <vedantvd07@gmail.com>
2025-07-19 08:29:26 +0530

    Time: 0 ms (100.00%) | Memory: 128.2 MB (79.45%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,23 +1,42 @@
 class Solution {
 public:
     // M-1 : First separating the pos and neg and then combining them
+    // vector<int> rearrangeArray(vector<int>& nums) {
+    //     vector<int> ans;
+    //     vector<int> pos;
+    //     vector<int> neg;
+
+    //     for(int i = 0; i < nums.size(); i++) {
+    //         if(nums[i] > 0) {
+    //             pos.push_back(nums[i]);
+    //         }
+    //         else {
+    //             neg.push_back(nums[i]);
+    //         }
+    //     }
+
+    //     for(int i = 0; i < pos.size(); i++) {
+    //         ans.push_back(pos[i]);
+    //         ans.push_back(neg[i]);
+    //     }
+
+    //     return ans;
+    // }
+
+    // M-2: Optimal
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int> ans;
-        vector<int> pos;
-        vector<int> neg;
+        vector<int> ans(nums.size());
+        int posInd = 0, negInd = 1;
 
         for(int i = 0; i < nums.size(); i++) {
             if(nums[i] > 0) {
-                pos.push_back(nums[i]);
+                ans[posInd] = nums[i];
+                posInd += 2;
             }
-            else {
-                neg.push_back(nums[i]);
+            else if(nums[i] < 0) {
+                ans[negInd] = nums[i];
+                negInd += 2;
             }
-        }
-
-        for(int i = 0; i < pos.size(); i++) {
-            ans.push_back(pos[i]);
-            ans.push_back(neg[i]);
         }
 
         return ans;
