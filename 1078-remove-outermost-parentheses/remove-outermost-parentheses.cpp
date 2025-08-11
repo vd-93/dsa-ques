e21cb272ca8a2f515297b9a8bff79dfedae965d4
vd-93 <vedantvd07@gmail.com>
2025-08-11 20:28:04 +0530

    Time: 4 ms (11.10%) | Memory: 8.8 MB (97.06%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,29 +1,57 @@
 class Solution {
 public:
-    // M-1 : O(2N)
+    // M-2 O(N)
+    /* We keep track of the balance between '(' and ')' using a counter.
+    We skip adding the first '(' and the last ')' of each primitive (when balance is 0
+    just before and just after).
+    */
     string removeOuterParentheses(string s) {
         string res;
-        int i = 0;
-        int openCnt = 0;
-        int closedCnt = 0;
-        for(int j = 0; j < s.size(); j++) {
-            if(s[j] == '(') {
-                openCnt++;
-            }
-            else {
-                closedCnt++;
-            }
+        int count = 0;
 
-            if(openCnt == closedCnt) {
-                for(int k = i + 1; k < j; k++) {
-                    res += s[k];
+        for (char ch : s) {
+            if (ch == '(') {
+                // skip outermost '('
+                if (count > 0) {
+                    res += ch;
                 }
-                openCnt = 0;
-                closedCnt = 0;
-                i = j + 1;
+                count++;
+            } else {
+                count--;
+                // skip outermost ')'
+                if (count > 0) {
+                    res += ch;
+                }
             }
         }
 
         return res;
     }
+
+    // M-1 : O(2N)
+    // string removeOuterParentheses(string s) {
+    //     string res;
+    //     int i = 0;
+    //     int openCnt = 0;
+    //     int closedCnt = 0;
+    //     for(int j = 0; j < s.size(); j++) {
+    //         if(s[j] == '(') {
+    //             openCnt++;
+    //         }
+    //         else {
+    //             closedCnt++;
+    //         }
+
+    //         if(openCnt == closedCnt) {
+    //             for(int k = i + 1; k < j; k++) {
+    //                 res += s[k];
+    //             }
+    //             openCnt = 0;
+    //             closedCnt = 0;
+    //             i = j + 1;
+    //         }
+    //     }
+
+    //     return res;
+    // }
 };
