23ac6aee53566f6d26820258921bf7199378f701
vd-93 <vedantvd07@gmail.com>
2025-08-11 11:53:46 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (60.54%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.cpp
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -10,36 +10,76 @@ edge cases :1) k == num's size -> return "0"
 */
 class Solution {
 public:
-    // M-1
+    // M-2 : Same logic diff code
     string removeKdigits(string num, int k) {
-        string res = "";
-        string ans = "";
+        stack<char> st;
 
         for(int i = 0; i < num.size(); i++) {
-            while(!res.empty() && k > 0 && (int)res.back() > (int)num[i]) {
-                res.pop_back();
+            while(!st.empty() && k > 0 && (int)st.top() > (int)num[i]) {
+                st.pop();
                 k--;
             }
-            res.push_back((char)num[i]);
+            st.push(num[i]);
         }
 
-        // Is k is still greater than 0, remove the last k elements
         while(k) {
-            res.pop_back();
+            st.pop();
             k--;
         }
 
-        bool leadingZeros = true;
-
-        for(char ch : res) {
-            if(leadingZeros && ch == '0') {
-                continue;
-            }
-            leadingZeros = false;
-            ans.push_back(ch);
+        if(st.empty()) {
+            return "0";
         }
 
-        if(ans.empty()) return "0";
-        return ans;
+        string res = "";
+
+        while(!st.empty()) {
+            res += st.top();
+            st.pop();
+        }
+
+        while(!res.empty() && res.back() == '0') {
+            res.pop_back();
+        }
+
+        if(res.empty()) {
+            return "0";
+        }
+
+        reverse(res.begin(), res.end());
+        return res;
     }
+
+    // M-1
+    // string removeKdigits(string num, int k) {
+    //     string res = "";
+    //     string ans = "";
+
+    //     for(int i = 0; i < num.size(); i++) {
+    //         while(!res.empty() && k > 0 && (int)res.back() > (int)num[i]) {
+    //             res.pop_back();
+    //             k--;
+    //         }
+    //         res.push_back((char)num[i]);
+    //     }
+
+    //     // Is k is still greater than 0, remove the last k elements
+    //     while(k) {
+    //         res.pop_back();
+    //         k--;
+    //     }
+
+    //     bool leadingZeros = true;
+
+    //     for(char ch : res) {
+    //         if(leadingZeros && ch == '0') {
+    //             continue;
+    //         }
+    //         leadingZeros = false;
+    //         ans.push_back(ch);
+    //     }
+
+    //     if(ans.empty()) return "0";
+    //     return ans;
+    // }
 };
