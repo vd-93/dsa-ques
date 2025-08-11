50fd538a6fe9339efef8cdd6009df338a1c98637
vd-93 <vedantvd07@gmail.com>
2025-08-11 08:21:07 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (90.14%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -1,6 +1,40 @@
 class Solution {
 public:
-    /*Logic:
+    /*M-2 - Same logic, just different code
+
+    here we are using vector instead of stack, to avoid extra reversing at the end
+    */
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> res;
+
+        for(int i = 0; i < asteroids.size(); i++) {
+            int currAst = asteroids[i];
+
+            if(currAst > 0) {
+                res.push_back(currAst);
+            }
+            // currAst < 0
+            else {
+                // currAst destroying smaller ast which are moving right
+                while(!res.empty() && res.back() > 0 && res.back() < abs(currAst)) {
+                    res.pop_back();
+                }
+                // curr ast and res.back() destroying each other
+                if(!res.empty() && res.back() == abs(currAst)) {
+                    res.pop_back();
+                }
+                // include curr negative ast only if the res is empty or the res.back is also negative
+                else if(res.empty() || res.back() < 0) {
+                    res.push_back(currAst);
+                }
+            }
+        }
+
+        return res;
+    }
+
+    /* M-1
+    Logic:
     traverse the asteroids array,
         - if the stack is empty or the stack's top and current ast are non colliding -> add to stack
         - else (stack top and curr ast are colliding)
@@ -11,50 +45,50 @@ public:
     at the end stack will be storing the asteroids, take them in array and reverse that array to get
     correct final state of asteroids
     */
-    bool nonColliding(int a, int b) {
-        return (a > 0 && b > 0) || (a < 0 && b < 0) || (a < 0 && b > 0);
-    }
-    vector<int> asteroidCollision(vector<int>& asteroids) {
-        vector<int> result;
-        stack<int> st;
+    // bool nonColliding(int a, int b) {
+    //     return (a > 0 && b > 0) || (a < 0 && b < 0) || (a < 0 && b > 0);
+    // }
+    // vector<int> asteroidCollision(vector<int>& asteroids) {
+    //     vector<int> result;
+    //     stack<int> st;
 
-        for(int i = 0; i < asteroids.size(); i++) {
-            int currAsteroid = asteroids[i];
+    //     for(int i = 0; i < asteroids.size(); i++) {
+    //         int currAsteroid = asteroids[i];
             
-            if(st.empty() || nonColliding(st.top(), currAsteroid)) {
-                st.push(currAsteroid);
-            }
-            // means curr ast is negative and st top is positive
-            else {
-                bool includeCurrAst = true;
+    //         if(st.empty() || nonColliding(st.top(), currAsteroid)) {
+    //             st.push(currAsteroid);
+    //         }
+    //         // means curr ast is negative and st top is positive
+    //         else {
+    //             bool includeCurrAst = true;
 
-                while(!st.empty() && !nonColliding(st.top(), currAsteroid)) {
-                    if(abs(currAsteroid) > st.top()) {
-                        st.pop();
-                    }
-                    else if(abs(currAsteroid) == st.top()) {
-                        st.pop();
-                        includeCurrAst = false;
-                        break;
-                    }
-                    else { // abs(currAsteroid < st.top())
-                        includeCurrAst = false;
-                        break;
-                    }
-                }
+    //             while(!st.empty() && !nonColliding(st.top(), currAsteroid)) {
+    //                 if(abs(currAsteroid) > st.top()) {
+    //                     st.pop();
+    //                 }
+    //                 else if(abs(currAsteroid) == st.top()) {
+    //                     st.pop();
+    //                     includeCurrAst = false;
+    //                     break;
+    //                 }
+    //                 else { // abs(currAsteroid < st.top())
+    //                     includeCurrAst = false;
+    //                     break;
+    //                 }
+    //             }
 
-                if(includeCurrAst) {
-                    st.push(currAsteroid);
-                }
-            }
-        }
+    //             if(includeCurrAst) {
+    //                 st.push(currAsteroid);
+    //             }
+    //         }
+    //     }
 
-        while(!st.empty()) {
-            result.push_back(st.top());
-            st.pop();
-        }
+    //     while(!st.empty()) {
+    //         result.push_back(st.top());
+    //         st.pop();
+    //     }
 
-        reverse(result.begin(), result.end());
-        return result;
-    }
+    //     reverse(result.begin(), result.end());
+    //     return result;
+    // }
 };
