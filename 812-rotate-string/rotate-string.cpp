3010c7f85ae2e8817e0a3dae6e384a0d6b99e9bb
vd-93 <vedantvd07@gmail.com>
2025-08-12 14:12:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (84.18%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
-    // M-1 : Try all possible rotations of s O(N^2)
+    // M-2 : Doubling the string s and checking if goal is present in s + s
     bool rotateString(string s, string goal) {
         if(s.size() != goal.size()) {
             return false;
         }
 
-        for(int i = 0; i < s.size(); i++) {
-            string rotated = s.substr(i) + s.substr(0, i);
+        string doubledString = s + s;
 
-            if(rotated == goal) {
-                return true;
-            }
-        }
-        return false;
+        return doubledString.find(goal) != string::npos;
     }
+
+    // M-1 : Try all possible rotations of s O(N^2)
+    // bool rotateString(string s, string goal) {
+    //     if(s.size() != goal.size()) {
+    //         return false;
+    //     }
+
+    //     for(int i = 0; i < s.size(); i++) {
+    //         string rotated = s.substr(i) + s.substr(0, i);
+
+    //         if(rotated == goal) {
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
 };
