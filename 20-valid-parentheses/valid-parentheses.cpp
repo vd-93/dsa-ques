364af01b39146cae3deb9470649e7efa04347abc
vd-93 <vedantvd07@gmail.com>
2025-08-13 15:10:55 +0530

    Time: 3 ms (6.87%) | Memory: 10 MB (5.44%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,43 +1,31 @@
 class Solution {
 public:
-    bool isOpeningBracket(char b) {
-        return b == '(' || b == '[' || b == '{';
+    bool matchingOpening(char open, char close) {
+        return (open == '(' && close == ')') || 
+            (open == '{' && close == '}') ||
+            (open == '[' && close == ']');
     }
-
-    bool matches(char a, char b) {
-        return (a == '(' && b == ')') || 
-               (a == '[' && b == ']') || 
-               (a == '{' && b == '}'); 
-    }
-
     bool isValid(string s) {
-        // If the string length is odd, it can't be a valid set of brackets
-        if (s.length() & 1) return false;   
+        if(s.size() & 1) {
+            return false;
+        }
+        
+        stack<int> st;
 
-        stack<char> st;
-
-        for (int i = 0; i < s.size(); i++) {
-            char ch = s[i];
-
-            if (isOpeningBracket(ch)) {
-                // Push opening brackets onto the stack
+        for(char ch : s) {
+            if(ch == '(' || ch == '{' || ch == '[') {
                 st.push(ch);
-            } else {
-                // If a closing bracket is found but the stack is empty, it's invalid
-                if (st.empty()) {
+            }
+            else {
+                if(st.empty() || !matchingOpening(st.top(), ch)) {
                     return false;
                 }
-
-                // Check if the top of the stack matches the current closing bracket
-                if (matches(st.top(), ch)) {
-                    st.pop(); // Valid match, pop the opening bracket
-                } else {
-                    return false; // Mismatch found
+                else {
+                    st.pop();
                 }
             }
         }
 
-        // If the stack is empty, all brackets were matched correctly
         return st.empty();
     }
 };
